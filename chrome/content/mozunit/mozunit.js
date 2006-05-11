de38e88e2b2b8b2c6a28846494550f1feee745d9
Massimiliano Mirra <bard@hyperstruct.net>
2006-05-11 13:33:36 +0200

    MozUnit: made TestSuite in class loadable by ModuleHelper.
    darcs-hash:20060511113336-8cc29-eda47d3329bbe19ca9707e9b057bbc7447b77738.gz

--- chrome/content/mozunit/mozunit.js
+++ chrome/content/mozunit/mozunit.js
@@ -16,180 +16,180 @@
 //
 // Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
 
-function TestSuite() {
+function constructor() {
     this.__tests = {}
+
+    this.__defineSetter__(
+        'tests', function(value) {
+            this.setTests(hash);
+        });
+
+    this.__defineGetter__(
+        'outputter', function() {
+            return this.__outputter;
+        });
+
+    this.__defineSetter__(
+        'outputter', function(fn) {
+            this.__outputter = fn;
+        });
+
+    this.__defineSetter__(
+        'stateThat', function(hash) {
+            this.setTests(hash);
+        });
 }
 
-TestSuite.prototype = {
-    setUp: function(fn) {
-        this.__setUp = fn;
-    },
-    
-    tearDown: function(fn) {
-        this.__tearDown = fn;
-    },
+function setUp(fn) {
+    this.__setUp = fn;
+}
 
-    test: function(desc, fn) {
-        this.__tests[desc] = fn;
-    },
+function tearDown(fn) {
+    this.__tearDown = fn;
+}
 
-    set tests(value) {
-        this.setTests(hash);        
-    },
+function test(desc, fn) {
+    this.__tests[desc] = fn;
+}
 
-    setTests: function(hash) {
-        // should probably clear our __tests hash and copy over, instead
-        // of modifying parameter
+function setTests(hash) {
+    // should probably clear our __tests hash and copy over, instead
+    // of modifying parameter
 
-        if(hash.setUp) {
-            this.__setUp = hash.setUp;
-            delete hash.setUp;
-        }
+    if(hash.setUp) {
+        this.__setUp = hash.setUp;
+        delete hash.setUp;
+    }
 
-        if(hash.given) {
-            this.__setUp = hash.given;
-            delete hash.given;
-        }
+    if(hash.given) {
+        this.__setUp = hash.given;
+        delete hash.given;
+    }
 
-        if(hash.tearDown) {
-            this.__tearDown = hash.tearDown;
-            delete hash.tearDown;
-        }
+    if(hash.tearDown) {
+        this.__tearDown = hash.tearDown;
+        delete hash.tearDown;
+    }
 
-        this.__tests = hash;
-    },
+    this.__tests = hash;
+}
 
-    testResult: function(eventType, eventLocation, message) {
-        if(eventType != 'SUCCESS')
-            this.__output(eventType + ' in <' + eventLocation + '>\n' + (message || '') + '\n');
-    },
+function testResult(eventType, eventLocation, message) {
+    if(eventType != 'SUCCESS')
+        this.__output(eventType + ' in <' + eventLocation + '>\n' + (message || '') + '\n');
+}
 
-    testSummary: function(summary) {
-        this.__output('\nTest run summary\n' +
-                      '  Successes: ' + summary.successes + '\n' +
-                      '  Failures:  ' + summary.failures + '\n' + 
-                      '  Errors:    ' + summary.errors + '\n\n');
-    },
+function testSummary(summary) {
+    this.__output('\nTest run summary\n' +
+                  '  Successes: ' + summary.successes + '\n' +
+                  '  Failures:  ' + summary.failures + '\n' +
+                  '  Errors:    ' + summary.errors + '\n\n');
+}
 
-    __output: function(string) {
-        if(this.__outputter)
-            this.__outputter(string);
-        else if(typeof(MozRepl_Server) == 'object' &&
-                MozRepl_Server.isActive())
-            MozRepl_dump(string);
-        else
-            dump(string);
-    },
-    
-    get outputter() {
-        return this.__outputter;
-    },
+function __output(string) {
+    if(this.__outputter)
+        this.__outputter(string);
+    else if(typeof(MozRepl_Server) == 'object' &&
+            MozRepl_Server.isActive())
+        MozRepl_dump(string);
+    else
+        dump(string);
+}
 
-    set outputter(fn) {
-        this.__outputter = fn;
-    },
+function run() {
+    var summary = {
+        successes: 0,
+        failures: 0,
+        errors: 0
+    };
 
-    run: function() {
-        var summary = {
-            successes: 0,
-            failures: 0,
-            errors: 0
-        };
+    for(desc in this.__tests) {
+        var context = {};
 
-        for(desc in this.__tests) {
-            var context = {};
+        try {
+            if(this.__setUp)
+                this.__setUp.call(context);
 
-            try {
-                if(this.__setUp)
-                    this.__setUp.call(context);
+            this.__tests[desc].call(context);
 
-                this.__tests[desc].call(context);
+            summary.successes += 1;
+            this.testResult('SUCCESS', desc);
+        } catch(exception if exception.name == 'AssertionFailed') {
+            summary.failures += 1;
 
-                summary.successes += 1;
-                this.testResult('SUCCESS', desc);
-            } catch(exception if exception.name == 'AssertionFailed') {
-                summary.failures += 1;
+            var message = '\t' + (exception.message || exception) + '\n';
 
-                var message = '\t' + (exception.message || exception) + '\n';
+            this.testResult('FAILURE', desc, message);
+        } catch(exception){
+            summary.errors += 1;
+            var trace = '';
 
-                this.testResult('FAILURE', desc, message);
-            } catch(exception){ 
-                summary.errors += 1;
-                var trace = '';
-                
-                if(exception.stack) {
-                    var calls = exception.stack.split('\n');
-                    for each (call in calls) {
-                        if(call.length > 0) {
-                            call = call.replace(/\\n/g, '\n');
-                            
-                            if(call.length > 200)
-                                call = call.substr(0, 200) + '[...]\n'
-                            
-                            trace += call.replace(/^/mg, '\t') + '\n';
-                        }
+            if(exception.stack) {
+                var calls = exception.stack.split('\n');
+                for each (call in calls) {
+                    if(call.length > 0) {
+                        call = call.replace(/\\n/g, '\n');
+
+                        if(call.length > 200)
+                            call = call.substr(0, 200) + '[...]\n'
+
+                                trace += call.replace(/^/mg, '\t') + '\n';
                     }
-                } 
-
-                trace = '\t' + exception.toString() + '\n' + trace;
-                this.testResult('ERROR', desc, trace);
+                }
             }
 
-            if(this.__tearDown)
-                this.__tearDown.call(context);
+            trace = '\t' + exception.toString() + '\n' + trace;
+            this.testResult('ERROR', desc, trace);
         }
-        this.testSummary(summary);
-    },
 
-    describe: function() {
-        this.__output('Specification\n\n')
+        if(this.__tearDown)
+            this.__tearDown.call(context);
+    }
+    this.testSummary(summary);
+}
+
+function describe() {
+    this.__output('Specification\n\n')
         for(desc in this.__tests)
             this.__output('  * ' + desc + '\n\n');
-    },
+}
 
-    /*
-     * The following aliases, together with the other name for
-     * TestSuite (Specification) encourage a more behavour-oriented
-     * (as opposed to test-oriented) frame of mind.
-     *
-     * Example:
-     *
-     *     var spec = new Specification();
-     *     
-     *     spec.stateThat = {
-     *         given: function() {
-     *             this.a = 3;
-     *         },
-     *     
-     *         'adding 1 and 2 is 3': function() {
-     *             Assert.equals(3, 1 + 2);
-     *         },
-     *     
-     *         'adding 3 and 4 is 7': function() {
-     *             Assert.equals(7, 3 + 4);
-     *         },
-     *     }
-     *     
-     *     spec.verify();
-     * 
-     */
+/*
+ * The following aliases, together with the other name for
+ * TestSuite (Specification) encourage a more behavour-oriented
+ * (as opposed to test-oriented) frame of mind.
+ *
+ * Example:
+ *
+ *     var spec = new Specification();
+ *
+ *     spec.stateThat = {
+ *         given: function() {
+ *             this.a = 3;
+ *         },
+ *
+ *         'adding 1 and 2 is 3': function() {
+ *             Assert.equals(3, 1 + 2);
+ *         },
+ *
+ *         'adding 3 and 4 is 7': function() {
+ *             Assert.equals(7, 3 + 4);
+ *         },
+ *     }
+ *
+ *     spec.verify();
+ *
+ */
 
-    given: function(fn) {
-        this.setUp(fn);
-    },
+function given(fn) {
+    this.setUp(fn);
+}
 
-    states: function(desc, fn) {
-        this.test(desc, fn);
-    },
+function states(desc, fn) {
+    this.test(desc, fn);
+}
 
-    verify: function() {
-        this.run();
-    },
+function verify() {
+    this.run();
+}
 
-    set stateThat(hash) {
-        this.setTests(hash);
-    }
-};
-
-Specification = TestSuite;
-    
