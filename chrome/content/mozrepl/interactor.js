4f8ed76a6711cd40442d2a1a971ef95b44057965
Massimiliano Mirra <bard@hyperstruct.net>
2006-05-11 12:46:08 +0200

    Migrated MozRepl to Module infrastructure.  Started migrating MozUnit
    darcs-hash:20060511104608-8cc29-9488f48b2c879380fb4f149db875f38868892da4.gz

--- chrome/content/mozrepl/interactor.js
+++ chrome/content/mozrepl/interactor.js
@@ -1,126 +1,122 @@
-// Copyright (C) 2006 by Massimiliano Mirra
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-//
-// Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+/*
+  Copyright (C) 2006 by Massimiliano Mirra
 
-function MozRepl_Interactor(instream, outstream) {
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+  Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+*/
+
+function constructor(instream, outstream) {
     this.__instream = instream;
     this.__outstream = outstream;
-};
+    this.__buffer == '';
+    this.__loader = Components
+        .classes['@mozilla.org/moz/jssubscript-loader;1']
+        .getService(Components.interfaces.mozIJSSubScriptLoader);
+    this.USE_SUBSCRIPT_LOADER_FOR_EVAL = false;
+}
 
-MozRepl_Interactor.prototype = {
-    __buffer: '',
-
-    __loader: Components
-    .classes['@mozilla.org/moz/jssubscript-loader;1']
-    .getService(Components.interfaces.mozIJSSubScriptLoader),
-
-    USE_SUBSCRIPT_LOADER_FOR_EVAL: false,
-    
-    eval: function(code) {
-        if(this.USE_SUBSCRIPT_LOADER_FOR_EVAL) {
-            dump('MozRepl: Evaluating as subscript: ' + code + '\n');
-            code = 'data:application/x-javascript,' + encodeURIComponent(code);
-            return this.load(code);
-        } else {
-            dump('MozRepl: Evaluating with eval(): ' + code + '\n');
-            return eval(code);
-        }
-    },
-
-    load: function(url, context) {
-        dump('MozRepl: Loading subscript ' + url + '\n');
-        return this.__loader.loadSubScript(url, context || window);    
-    },
-
-    output: function(text) {
-        this.__outstream.write(text, text.length);
-    },
-
-    onStartRequest: function(request, context) {
-        this.name = (new Date()).getTime()
-        MozRepl_Server.addInteractor(this, this.name);
-    },
-
-    onStopRequest: function(request, context, status) {
-        this.__instream.close();
-        this.__outstream.close();
-        if(this.name)
-            MozRepl_Server.removeInteractor(name);
-        dump('MozRepl: Closed a connection.\n');
-    },
-
-    onDataAvailable: function(request, context, inputStream, offset, count) {
-        try {
-            this.__buffer += this.__instream.read(count);
-
-            var rx = /^::([^\s]+) (.+)\n/m;
-            m = this.__buffer.match(rx);
-            if(m) {
-                var cmd = m[1];
-                var arg = m[2];
-                this.__buffer = this.__buffer.replace(rx, '');
-            
-                switch(cmd) {
-                case 'name':
-                    var oldName = this.name;
-                    this.name = arg;
-                    MozRepl_Server.renameInteractor(oldName, this.name);
-                    break;
-                case 'echo':
-                    this.__outstream.write(arg + '\n', arg.length + 1);
-                    break;
-                case 'load':
-                    var result = this.load(arg) + '\n\n';
-                    this.__outstream.write('>>> ' + result, result.length + 4);
-                    break;
-                }
-            }
-
-            var match = this.__buffer.match(/\n--end-emacs-input\n/m);
-            if (match) {
-                var code = this.__buffer.substr(0, match.index);
-                this.__buffer = '';
-
-                var result = this.eval(code) + '\n\n';
-                this.__outstream.write('>>> ' + result, result.length + 4);
-            }
-
-        } catch(exception) {
-            var trace = '';
-                
-            if(exception.stack) {
-                var calls = exception.stack.split('\n');
-                for each (call in calls) {
-                    if(call.length > 0) {
-                        call = call.replace(/\\n/g, '\n');
-                            
-                        if(call.length > 200)
-                            call = call.substr(0, 200) + '[...]\n'
-                            
-                                trace += call.replace(/^/mg, '\t') + '\n';
-                    }
-                }
-            }
-
-            trace +=  '!!! ' + exception.toString() + '\n\n';
-                    
-            this.output(trace);
-            this.__buffer = '';
-        }
+function evaluate(code) {
+    if(this.USE_SUBSCRIPT_LOADER_FOR_EVAL) {
+        dump('MozRepl: Evaluating as subscript: ' + code + '\n');
+        code = 'data:application/x-javascript,' + encodeURIComponent(code);
+        return this.load(code);
+    } else {
+        dump('MozRepl: Evaluating with eval(): ' + code + '\n');
+        return eval(code);
     }
 }
 
+function load(url, context) {
+    dump('MozRepl: Loading subscript ' + url + '\n');
+    return this.__loader.loadSubScript(url, context || window);    
+}
+
+function output(text) {
+    this.__outstream.write(text, text.length);
+}
+
+function onStartRequest(request, context) {
+//        this.name = (new Date()).getTime()
+//        MozRepl_Server.addInteractor(this, this.name);
+}
+
+function onStopRequest(request, context, status) {
+    this.__instream.close();
+    this.__outstream.close();
+//        if(this.name)
+//            MozRepl_Server.removeInteractor(name);
+    dump('MozRepl: Closed a connection.\n');
+}
+
+function onDataAvailable(request, context, inputStream, offset, count) {
+    try {
+        this.__buffer += this.__instream.read(count);
+
+        var rx = /^::([^\s]+) (.+)\n/m;
+        m = this.__buffer.match(rx);
+        if(m) {
+            var cmd = m[1];
+            var arg = m[2];
+            this.__buffer = this.__buffer.replace(rx, '');
+            
+            switch(cmd) {
+            case 'name':
+                var oldName = this.name;
+                this.name = arg;
+                MozRepl_Server.renameInteractor(oldName, this.name);
+                break;
+            case 'echo':
+                this.__outstream.write(arg + '\n', arg.length + 1);
+                break;
+            case 'load':
+                var result = this.load(arg) + '\n\n';
+                this.__outstream.write('>>> ' + result, result.length + 4);
+                break;
+            }
+        }
+
+        var match = this.__buffer.match(/\n--end-emacs-input\n/m);
+        if (match) {
+            var code = this.__buffer.substr(0, match.index);
+            this.__buffer = '';
+
+            var result = this.evaluate(code) + '\n\n';
+            this.__outstream.write('>>> ' + result, result.length + 4);
+        }
+
+    } catch(exception) {
+        var trace = '';
+                
+        if(exception.stack) {
+            var calls = exception.stack.split('\n');
+            for each (call in calls) {
+                if(call.length > 0) {
+                    call = call.replace(/\\n/g, '\n');
+                            
+                    if(call.length > 200)
+                        call = call.substr(0, 200) + '[...]\n'
+                            
+                            trace += call.replace(/^/mg, '\t') + '\n';
+                }
+            }
+        }
+
+        trace +=  '!!! ' + exception.toString() + '\n\n';
+                    
+        this.output(trace);
+        this.__buffer = '';
+    }
+}
--- chrome/content/mozrepl/server.js
+++ chrome/content/mozrepl/server.js
@@ -1,25 +1,33 @@
-// Copyright (C) 2006 by Massimiliano Mirra
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-//
-// Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+/*
+  Copyright (C) 2006 by Massimiliano Mirra
 
-var MozRepl_Server = {
-    Port: 4242,
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    socketListener: {
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+  Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+*/
+
+var Interactor = Module.require('class', 'interactor');
+
+function constructor(port) {
+    this._port = 4242 || port;
+}
+
+function start() {
+    var server = this;
+
+    var socketListener = {
         onSocketAccepted: function(serv, transport) {
             try {
                 var outstream = transport.openOutputStream(0, 0, 0);
@@ -34,73 +42,77 @@ var MozRepl_Server = {
             }
             dump('MozRepl: Accepted connection.\n');
 
+            var interactor = new Interactor(instream, outstream)
+            interactor.name = (new Date()).getTime();
+
             var pump = Components
             .classes['@mozilla.org/network/input-stream-pump;1']
             .createInstance(Components.interfaces.nsIInputStreamPump);
+
             pump.init(stream, -1, -1, 0, 0, false);
-            pump.asyncRead(new MozRepl_Interactor(instream, outstream), null);
+            pump.asyncRead(interactor, null);
+            server.addInteractor(interactor, interactor.name);
+        },
+        onStopListening: function(serv, status) {
+            alert('stop listening');
         }
-    },
+    };
 
-    start: function() {
-        this.__interactors = {};
-        try {
-            this.__serv = Components
-                .classes['@mozilla.org/network/server-socket;1']
-                .createInstance(Components.interfaces.nsIServerSocket);
-            this.__serv.init(this.Port, true, -1);
-            this.__serv.asyncListen(this.socketListener);
-            dump('MozRepl: Listening...\n');
-        } catch(e) {
-            dump('MozRepl: Exception: ' + e);
-        }
-    },
-
-    stop: function() {
-        dump('MozRepl: Closing...\n');
-        this.__serv.close();
-        delete this.__serv;
-        delete this.__interactors;
-    },
-    
-    isActive: function() {
-        if(this.__serv)
-            return true;
-    },
-    
-    addInteractor: function(interactor, name) {
-        if(!this.__interactors[name])
-            this.__interactors[name] = interactor;
-    },
-
-    renameInteractor: function(oldName, newName) {
-        this.__interactors[newName] = this.__interactors[oldName];
-        delete this.__interactors[oldName];
-    },
-
-    removeInteractor: function(name) {
-        delete this.__interactors[name];
-    },
-
-    getInteractor: function(name) {
-        return this.__interactors[name];
-    },
-
-    getFirstInteractor: function() {
-        for(name in this.__interactors)
-            return this.__interactors[name];
-        return null;
-    }
-};
-
-// ----------------------------------------------------------------------
-
-function MozRepl_dump(text, interactorName) {
-    var inteactor;
-    if(interactorName)
-        interactor = MozRepl_Server.getInteractor(interactorName);
-    else
-        interactor = MozRepl_Server.getFirstInteractor();
-
-    interactor.output(text);
+    this.__interactors = {};
+    try {
+        this.__serv = Components
+            .classes['@mozilla.org/network/server-socket;1']
+            .createInstance(Components.interfaces.nsIServerSocket);
+        this.__serv.init(this._port, true, -1);
+        this.__serv.asyncListen(socketListener);
+        dump('MozRepl: Listening...\n');
+    } catch(e) {
+        dump('MozRepl: Exception: ' + e);
+    }    
 }
+
+function stop() {
+    dump('MozRepl: Closing...\n');
+    this.__serv.close();
+    delete this.__serv;
+    delete this.__interactors;
+}
+ 
+function isActive() {
+    if(this.__serv)
+        return true;
+}
+ 
+function addInteractor(interactor, name) {
+    if(!this.__interactors[name])
+        this.__interactors[name] = interactor;
+}
+
+function renameInteractor(oldName, newName) {
+    this.__interactors[newName] = this.__interactors[oldName];
+    delete this.__interactors[oldName];
+}
+
+function removeInteractor(name) {
+    delete this.__interactors[name];
+}
+
+function getInteractor(name) {
+    return this.__interactors[name];
+}
+    
+function getFirstInteractor() {
+    for(name in this.__interactors)
+        return this.__interactors[name];
+    return null;
+}
+
+// function dump(text, interactorName) {
+//     var inteactor;
+//     if(interactorName)
+//         interactor = MozRepl_Server.getInteractor(interactorName);
+//     else
+//         interactor = MozRepl_Server.getFirstInteractor();
+    
+//     interactor.output(text);
+// }
