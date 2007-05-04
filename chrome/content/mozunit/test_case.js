d22163cba0488c66b4c5bf78d7f8ef2d599cd2a0
Massimiliano Mirra <bard@hyperstruct.net>
2007-05-05 00:34:09 +0200

    Renamed overlays to more sensible names.
    darcs-hash:20070504223409-8cc29-a2feed4207bdfd0e59ffd47ad15763fa62679215.gz

--- chrome/content/mozunit/test_case.js
+++ chrome/content/mozunit/test_case.js
@@ -335,6 +335,7 @@ function _asyncRun1(tests, setUp, tearDown, reportHandler, onTestRunFinished) {
 
     fsm.go('start', {}, stateHandlers, stateTransitions, []);
 }
+}
 
 function _defaultReportHandler(report) {
     if(report.result == 'success')
