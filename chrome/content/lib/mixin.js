3fd2dd90ea3090c0a819af089ab0aeb4df7b7ef7
Massimiliano Mirra <bard@hyperstruct.net>
2007-05-04 23:58:51 +0200

    Removed all libraries except module_manager, which is needed by mozunit and mozrepl.
    darcs-hash:20070504215851-8cc29-2bdd4797341476d8b6f213e5ddfd1e144c2c7819.gz

--- chrome/content/lib/mixin.js
+++
@@ -1,8 +0,0 @@
-function forward(source, messageName, destination) {
-    source[messageName] = function() {
-        // TODO: leave option to choose in which context the
-        // forwarded method will run
-        return destination[messageName].apply(destination, arguments) 
-    }
-}
-
