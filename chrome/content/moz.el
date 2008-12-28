dee0dd098cb80580699d376d54f8ae206b048c68
Massimiliano Mirra <bard@hyperstruct.net>
2008-12-28 19:23:45 +0100

    moz.el: added doc strings, mode map, commentary, and error handler.  (Contributed by Lennart Borgman.)

--- chrome/content/moz.el
+++ chrome/content/moz.el
@@ -1,5 +1,7 @@
 ;;; moz.el --- Lets current buffer interact with inferior mozilla.
 
+;; URL: http://github.com/bard/mozrepl/raw/master/chrome/content/moz.el
+
 ;; Copyright (C) 2006 by Massimiliano Mirra
 ;;
 ;; This program is free software; you can redistribute it and/or modify
@@ -17,28 +19,94 @@
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 ;;
 ;; Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+;; Contributors:
+;;   - Lennart Borgman
+
+;;; Commentary:
+;;
+;; This file implements communication with Firefox via MozRepl
+;; (http://hyperstruct.net/projects/mozrepl).  It is a slightly
+;; modified version of the file moz.el that comes with MozLab.  To use
+;; it you have to install the MozRepl addon in Firefox.
+;;
+;; This file contains
+;;
+;;   * a major mode for direct interaction in a buffer (as with
+;;     telnet) with MozRepl, `inferior-moz-mode'.
+;;   * a minor mode for sending code portions or whole files from
+;;     other buffers to MozRepl, `moz-minor-mode'.
+;;
+;; Assuming you want to use javascript-mode to edit Javascript files,
+;; enter the following in your .emacs initialization file (from Emacs
+;; integration in the help text):
+;;
+;;   (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
+;;   (autoload 'inferior-moz-mode "moz" "MozRepl Inferior Mode" t)
+;;   (autoload 'moz-minor-mode "moz" "MozRepl Minor Mode" t)
+;;   (add-hook 'javascript-mode-hook 'javascript-moz-setup)
+;;   (defun javascript-moz-setup () (moz-minor-mode 1))
+;;
+;; Replace javascript-mode above with the name of your favorite
+;; javascript mode.
+;;
+;; If you got this with nXhtml the setup above is already done for
+;; you.
+;;
+;; *Note 1* You have to start the MozRepl server in Firefox (or
+;;  whatever Mozilla browser you use). From the menus do
+;;
+;;    Tools - MozRepl - Start
+;;
+;; *Note 2* For clearness and brevity the documentation says Firefox
+;;  where the correct term should rather be "your Mozilla web
+;;  browser".
+
+;;; Change log:
+;;
+;; 2008-07-20: Lennart Borgman
+;;  - Add `moz-minor-mode-map'.
+;;  - Add `inferior-moz-insert-moz-repl'.
+;;  - Add `inferior-moz-mode-map'.
+;;  - Add doc strings to interactive functions.
+;;  - Make minor enhancements to documentation etc.
+;;  - Change Mozilla to Firefox/MozRepl for clarity and brevity.
+;;  - Add error handling when starting MozRepl.
 
 ;;; Code:
 
 (require 'comint)
 
+;; Maybe fix-me: C-c control-char are reserved for major modes. But
+;; this minor mode is used in only one major mode (or one family of
+;; major modes) so it complies I think ...
+(defvar moz-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\C-c\C-s" 'run-mozilla)
+    (define-key map "\C-\M-x"  'moz-send-defun)
+    (define-key map "\C-c\C-c" 'moz-send-defun-and-go)
+    (define-key map "\C-c\C-r" 'moz-send-region)
+    (define-key map "\C-c\C-l" 'moz-save-buffer-and-send)
+    map))
+
+;;;###autoload
 (define-minor-mode moz-minor-mode
-  "Toggle Mozilla mode.
+  "MozRepl minor mode for interaction with Firefox.
 With no argument, this command toggles the mode.
 Non-null prefix argument turns on the mode.
 Null prefix argument turns off the mode.
 
-When Mozilla mode is enabled, some commands become available to
-send current code area (as understood by c-mark-function) or
-region or buffer to an inferior mozilla process (which will be
-started as needed)."
+When this minor mode is enabled, some commands become available
+to send current code area \(as understood by c-mark-function) or
+region or buffer to an inferior MozRepl process (which will be
+started as needed).
+
+The following keys are bound in this minor mode:
+
+\\{moz-minor-mode-map}"
   nil
   " Moz"
-  '(("\C-c\C-s" . run-mozilla)
-    ("\C-\M-x"  . moz-send-defun)
-    ("\C-c\C-c" . moz-send-defun-and-go)
-    ("\C-c\C-r" . moz-send-region)
-    ("\C-c\C-l" . moz-save-buffer-and-send)))
+  :keymap moz-minor-mode-map
+  :group 'moz)
 
 (defalias 'run-mozilla 'inferior-moz-switch-to-mozilla)
 
@@ -58,6 +126,7 @@ started as needed)."
     (setq moz-temporary-file (make-temp-file "emacs-mozrepl"))))
 
 (defun moz-send-region (start end)
+  "Send the region to Firefox via MozRepl."
   (interactive "r")
   (comint-send-string (inferior-moz-process)
                       (concat moz-repl-name ".pushenv('printPrompt', 'inputMode'); "
@@ -79,17 +148,22 @@ started as needed)."
   (display-buffer (process-buffer (inferior-moz-process))))
 
 (defun moz-send-defun ()
+  "Send the current function to Firefox via MozRepl.
+Curent function is the one recognized by c-mark-function."
   (interactive)
   (save-excursion
     (c-mark-function)
     (moz-send-region (point) (mark))))
 
 (defun moz-send-defun-and-go ()
+  "Send the current function to Firefox via MozRepl.
+Also switch to the interaction buffer."
   (interactive)
   (moz-send-defun)
   (inferior-moz-switch-to-mozilla))
 
 (defun moz-save-buffer-and-send ()
+  "Save the current buffer and load it in Firefox via MozRepl."
   (interactive)
   (save-buffer)
   (comint-send-string (inferior-moz-process)
@@ -106,12 +180,21 @@ started as needed)."
 (defvar inferior-moz-buffer nil
   "The buffer in which the inferior process is running.")
 
-(define-derived-mode inferior-moz-mode comint-mode "Inf-Mozilla"
-  "Major mode for interacting with a Mozilla browser."
+(defun inferior-moz-insert-moz-repl ()
+  "Insert value of `moz-repl-name' and a dot (.)."
+  (interactive) (insert moz-repl-name "."))
+
+(defvar inferior-moz-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\C-cc" 'inferior-moz-insert-moz-repl)
+    map))
+
+;;;###autoload
+(define-derived-mode inferior-moz-mode comint-mode "Inf-MozRepl"
+  "Major mode for interacting with Firefox via MozRepl."
   (setq comint-input-sender 'inferior-moz-input-sender)
-  (define-key inferior-moz-mode-map "\C-cc" (lambda () (interactive) (insert moz-repl-name ".")))
   (add-hook 'comint-output-filter-functions 'inferior-moz-track-repl-name nil t))
-            
+
 (defun inferior-moz-track-repl-name (comint-output)
   (when (string-match "\\(\\w+\\)> $" comint-output)
     (setq moz-repl-name (match-string 1 comint-output))))
@@ -129,9 +212,13 @@ comint-simple-send, here we *first* concatenate input and
 newline, then send it all together.  This prevents newline to be
 interpreted on its own."
   (comint-send-string proc (concat string "\n")))
-    
+
 (defun inferior-moz-switch-to-mozilla (arg)
-  "Show the inferior mozilla buffer.  Start the process if needed."
+  "Switch to the inferior MozRepl buffer.
+Create the buffer and start the MozRepl process and connect to
+Firefox if needed.
+
+See also `inferior-moz-start-process'."
   (interactive "P")
   (when arg
     (setq moz-repl-host (read-string "Host: " "localhost"))
@@ -140,7 +227,8 @@ interpreted on its own."
   (goto-char (process-mark (inferior-moz-process))))
 
 (defun inferior-moz-process ()
-  "Return inferior mozilla process.  Start it if necessary."
+  "Return inferior MozRepl process.  Start it if necessary.
+See also `inferior-moz-start-process'."
   (or (if (buffer-live-p inferior-moz-buffer)
           (get-buffer-process inferior-moz-buffer))
       (progn
@@ -148,16 +236,46 @@ interpreted on its own."
         (inferior-moz-process))))
 
 (defun inferior-moz-start-process ()
-  "Start an inferior mozilla process.
+  "Start an inferior Mozrepl process and connect to Firefox.
 It runs the hook `inferior-moz-hook' after starting the process
-and setting up the inferior-mozilla buffer."
+and setting up the inferior Firefox buffer.
+
+Note that you have to start the MozRepl server from Firefox."
   (interactive)
-  (setq inferior-moz-buffer
-        (apply 'make-comint "Moz" (cons moz-repl-host moz-repl-port) nil nil))
-  (sleep-for 0 100)
-  (with-current-buffer inferior-moz-buffer
-    (inferior-moz-mode)
-    (run-hooks 'inferior-moz-hook)))
+  (condition-case err
+      (progn
+        (setq inferior-moz-buffer
+              (apply 'make-comint "MozRepl" (cons moz-repl-host moz-repl-port) nil nil))
+        (sleep-for 0 100)
+        (with-current-buffer inferior-moz-buffer
+          (inferior-moz-mode)
+          (run-hooks 'inferior-moz-hook)))
+    (file-error
+     (with-output-to-temp-buffer "*MozRepl Error*"
+       (with-current-buffer (get-buffer "*MozRepl Error*")
+         (insert "Can't start MozRepl, the error message was:\n\n     "
+                 (error-message-string err)
+                 "\n"
+                 "\nA possible reason is that you have not installed"
+                 "\nthe MozRepl add-on to Firefox or that you have not"
+                 "\nstarted it.  You start it from the menus in Firefox:"
+                 "\n\n     Tools / MozRepl / Start"
+                 "\n"
+                 "\nSee ")
+         (insert-text-button
+          "MozRepl home page"
+          'action (lambda (button)
+                    (browse-url
+                     "http://hyperstruct.net/projects/mozrepl")
+                    )
+          'face 'button)
+         (insert
+          " for more information."
+          "\n"
+          "\nMozRepl is also available directly from Firefox add-on"
+          "\npages, but is updated less frequently there.")
+         ))
+     (error "Can't start MozRepl"))))
 
 (provide 'moz)
 
