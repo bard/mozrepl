6bc078f6206224d6a021ee534bdad2018d167db5
Ethan <ethan@unth>
2011-04-24 22:28:04 -0400

    change what is emitted to the socket to utf8

--- chrome/content/server.js
+++ chrome/content/server.js
@@ -96,6 +96,9 @@ function start(port, loopbackOnly) {
 function onSocketAccepted(serv, transport) {
     try {
         var outstream = transport.openOutputStream(Ci.nsITransport.OPEN_BLOCKING , 0, 0);
+        var outstreamutf8 = Cc['@mozilla.org/intl/converter-output-stream;1']
+            .createInstance(Ci.nsIConverterOutputStream);
+        outstreamutf8.init(outstream, 'UTF-8', 0, 0);
 
         var stream = transport.openInputStream(0, 0, 0);
         var instream = Cc['@mozilla.org/intl/converter-input-stream;1']
@@ -118,7 +121,7 @@ function onSocketAccepted(serv, transport) {
 
     var session = new REPL();
     session.onOutput = function(string) {
-        outstream.write(string, string.length);
+        outstreamutf8.writeString(string);
     };
     session.onQuit = function() {
         log('I, MOZREPL : Client closed connection : ' + transport.host + ':' + transport.port);        
