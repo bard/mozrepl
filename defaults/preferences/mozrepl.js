d58bb2eeeddca79610355979a2115b64dceff898
d-faure <dominique.faure@gmail.com>
2013-06-11 12:02:23 +0200

    Added option to disable default message logs

--- defaults/preferences/mozrepl.js
+++ defaults/preferences/mozrepl.js
@@ -6,3 +6,4 @@ pref("extensions.mozrepl.version", "");
 pref("extensions.mozrepl.defaultInteractor", "javascript");
 pref("extensions.mozrepl.interactor.javascript.printWelcome", true);
 pref("extensions.mozrepl.startingContext", "");
+pref("extensions.mozrepl.noLog", false);
